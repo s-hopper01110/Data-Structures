f7baa9ad38a696e555820eb5d136914f466fc1a6
Sheila <s.hopper01110@gmail.com>
2019-05-15 16:45:15 -0400

    BST-1

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,13 +5,28 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if value < self.value:
+      if not self.left:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
+
+    elif value >= self.value:
+      if not self.right:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
 
   def contains(self, target):
     pass
 
   def get_max(self):
-    pass
+    current = self
+
+    while(current.right):
+      current = current.right
+    return current.value
+    
 
   def for_each(self, cb):
     pass
